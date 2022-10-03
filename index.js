a01d88809985f147144db2eedca4e84599eb5c22
sawfiz <dashu.up@gmail.com>
2022-10-03 21:36:51 +0800

    Streamline add book to library. Add read status icon and toggle

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const newBookAuthorEl = document.querySelector("#author");
 const newBookPagesEl = document.querySelector("#pages");
 const newBookReadEl = document.querySelector("#read");
 const addBookBtnEl = document.querySelector("#addBookBtn");
+const readEls = Array.from(document.querySelectorAll(".read"))
 
 const library = [];
 
@@ -18,32 +19,39 @@ function addBookToLibrary(book) {
     library.push(book);
 }
 
+function addBookToDisplay(book) {
+    const bookRow = document.createElement("tr");
+    const bookTitle = document.createElement("td");
+    bookTitle.innerText = book.title;
+    bookRow.appendChild(bookTitle);
+    const bookAuthor = document.createElement("td");
+    bookAuthor.innerText = book.author;
+    bookRow.appendChild(bookAuthor);
+    const bookPages = document.createElement("td");
+    bookPages.innerText = book.pages;
+    bookRow.appendChild(bookPages);
+    const bookRead = document.createElement("td");
+    bookRead.innerHTML = book.isRead === true ? '<img src="images/check.svg" class="read">' : '<img src="images/uncheck.svg" class="read">';
+    bookRow.appendChild(bookRead);
+    bookRead.addEventListener("click", () => {
+        if (book.isRead) {
+            bookRead.innerHTML = '<img src="images/uncheck.svg" class="read">'
+            book.isRead = false
+        } else {
+            bookRead.innerHTML = '<img src="images/check.svg" class="read">'
+            book.isRead = true
+        }
+    })
+    tableBodyEl.appendChild(bookRow);
+}
+
 function changeReadStatus(book) {
     book.isRead = book.isRead === true ? false : true;
 }
 
 function displayLibrary() {
-    let index = 0;
     library.forEach((book) => {
-        console.log(book);
-        index++;
-        const bookRow = document.createElement("tr");
-        const bookIndex = document.createElement("td");
-        bookIndex.innerText = index;
-        bookRow.appendChild(bookIndex);
-        const bookTitle = document.createElement("td");
-        bookTitle.innerText = book.title;
-        bookRow.appendChild(bookTitle);
-        const bookAuthor = document.createElement("td");
-        bookAuthor.innerText = book.author;
-        bookRow.appendChild(bookAuthor);
-        const bookPages = document.createElement("td");
-        bookPages.innerText = book.pages;
-        bookRow.appendChild(bookPages);
-        const bookRead = document.createElement("td");
-        bookRead.innerText = book.isRead;
-        bookRow.appendChild(bookRead);
-        tableBodyEl.appendChild(bookRow);
+        addBookToDisplay(book);
     });
 }
 
@@ -54,6 +62,16 @@ function clearLibraryDisplay() {
         "<tr><th>Index</th><th>Title</th><th>Author</th><th>Pages</th><th>Read</th></tr>";
     tableBodyEl.appendChild(headerRow);
 }
+
+function clearInputs() {
+    newBookTitleEl.value = "";
+    newBookAuthorEl.value = "";
+    newBookPagesEl.value = undefined;
+    newBookReadEl.value = false;
+}
+
+
+// main program
 
 const lotr1 = new Book("LOTR1", "Tolkin", 344, false);
 const lotr2 = new Book("LOTR2", "Tolkin", 550, false);
@@ -70,7 +88,7 @@ addBookBtnEl.addEventListener("click", (e) => {
     if (
         newBookTitleEl.value !== "" &&
         newBookAuthorEl.value !== "" &&
-        newBookPagesEl.value > 1
+        newBookPagesEl.value >= 1
     ) {
         const newBook = new Book(
             newBookTitleEl.value,
@@ -79,14 +97,9 @@ addBookBtnEl.addEventListener("click", (e) => {
             newBookReadEl.checked
         );
         addBookToLibrary(newBook);
-        console.log(newBookTitleEl);
-        clearLibraryDisplay();
-        displayLibrary();
-        newBookTitleEl.value = "";
-        newBookAuthorEl.value = "";
-        newBookPagesEl.value = undefined;
+        addBookToDisplay(newBook);
+        clearInputs();
     } else {
         e.preventDefault();
-
     }
 });
